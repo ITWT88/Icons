9f8c18e2de9211dceaa99fab59c4c09b29eb6d96
ITWT88 <82963577+ITWT88@users.noreply.github.com>
2021-04-22 14:23:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
   if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('https://raw.githubusercontent.com/ITWT88/Icons/main/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('https://rawcdn.githack.com/ITWT88/Icons/9882c4c1ad39ade2d80bd33c88431d89071d2e5e/sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
